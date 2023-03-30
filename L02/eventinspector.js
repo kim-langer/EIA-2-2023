21e16fbcd3ddae1e6bbeb35d67cb16924212acfb
kim-langer <kim.sophie.langer@hs-furtwangen.de>
2023-03-30 13:56:07 +0200

    zs

--- L02/eventinspector.js
+++ L02/eventinspector.js
@@ -1,1 +1,6 @@
+//install load listener//
+window.addEventListener('load', function () {
+    // Hier können Sie den Code platzieren, der ausgeführt werden soll, wenn die Seite vollständig geladen ist.
+    console.log('Die Seite wurde vollständig geladen.');
+});
 //# sourceMappingURL=eventinspector.js.map
---
+++ L02/eventinspector.ts
@@ -0,0 +1,7 @@
+
+//install load listener//
+window.addEventListener('load', function() {
+    // Hier können Sie den Code platzieren, der ausgeführt werden soll, wenn die Seite vollständig geladen ist.
+    console.log('Die Seite wurde vollständig geladen.');
+  });
+  
