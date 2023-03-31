42dfeb40661592280ae3dc77b616251948c0ecd5
kim-langer <kim.sophie.langer@hs-furtwangen.de>
2023-03-31 18:17:05 +0200

    zs

--- L02/eventinspector.ts
+++ L02/eventinspector.ts
@@ -1,7 +1,38 @@
 
-//install load listener//
+//Die Seite soll erst vollständig geladen werden//
 window.addEventListener('load', function() {
-    // Hier können Sie den Code platzieren, der ausgeführt werden soll, wenn die Seite vollständig geladen ist.
-    console.log('Die Seite wurde vollständig geladen.');
+
+  //Die Funktion, die auf Aktiviäten auf der Seite hört//
+  function handleload() {
+    document.addEventListener("mousemove", setInfoBox);
+    document.addEventListener("click", logInfo);
+    document.addEventListener("keyup", logInfo);
+  
+    const body = document.getElementsByTagName("body")[0];
+    body.addEventListener("click", logInfo);
+    body.addEventListener("keyup", logInfo);
+  
+      //Schleife, die sicherstellt, dass auf beide Div Elemente reagiert wird//
+    const divs = document.getElementsByClassName("divs");
+    for (let i = 0; i < divs.length; i++) {
+      divs[i].addEventListener("click", logInfo);
+      divs[i].addEventListener("keyup", logInfo);
+    }
+  }
+  
+
+hi.addeventlistener ("click", function setinfoBox (_event:MouseEvent) {
+
+
+    });
+
+    function logInfo () {
+
+
+    };
+
+
+
+
   });
   
