d2e7aeef777bf6b53792686dd1a296d4e2f6427e
kim-langer <kim.sophie.langer@hs-furtwangen.de>
2023-03-31 19:17:36 +0200

    zs

--- L02/eventinspector.js
+++ L02/eventinspector.js
@@ -1,6 +1,45 @@
-//install load listener//
+/*Aufgabe: L02
+Name: Kim Langer
+Matrikelnummer: 27174
+*/
+//Die Seite soll erst vollständig geladen werden//
 window.addEventListener('load', function () {
-    // Hier können Sie den Code platzieren, der ausgeführt werden soll, wenn die Seite vollständig geladen ist.
-    console.log('Die Seite wurde vollständig geladen.');
+    //globale Variablen//
+    let infobox = document.getElementById("thespan");
+    //Die Funktion, die auf Aktiviäten auf der Seite hört//
+    function handleload() {
+        document.addEventListener("mousemove", setinfoBox);
+        document.addEventListener("click", logInfo);
+        document.addEventListener("keyup", logInfo);
+        const body = document.getElementsByTagName("body")[0];
+        body.addEventListener("click", logInfo);
+        body.addEventListener("keyup", logInfo);
+        //Schleife, die sicherstellt, dass auf beide Div Elemente reagiert wird//
+        const divs = document.getElementsByClassName("divs");
+        for (let i = 0; i < divs.length; i++) {
+            divs[i].addEventListener("click", logInfo);
+            divs[i].addEventListener("keyup", logInfo);
+        }
+    }
+    ;
+    //Inhalt des Span-Elements anzeigen//
+    function setinfoBox(_event) {
+        const positionx = _event.clientX;
+        const positiony = _event.clientY;
+        let x = String(positionx + 15);
+        let y = String(positiony + 15);
+        let infobox = document.getElementById("thespan");
+        infobox.innerHTML = "position x: " + x + " position y: " + y + " Event-Target: " + _event.target;
+        infobox.style.position = "fixed";
+        infobox.style.top = y + "px";
+        infobox.style.left = x + "px";
+    }
+    ;
+    //Infos in der Konsole aufrufen//
+    function logInfo(_event) {
+        console.log(_event.type + _event.target + _event.currentTarget + _event);
+    }
+    ;
+    handleload();
 });
 //# sourceMappingURL=eventinspector.js.map
--- L02/eventinspector.ts
+++ L02/eventinspector.ts
@@ -1,38 +1,55 @@
+/*Aufgabe: L02
+Name: Kim Langer
+Matrikelnummer: 27174
+*/
 
 //Die Seite soll erst vollständig geladen werden//
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
+
+  //globale Variablen//
+  let infobox = document.getElementById("thespan") as HTMLSpanElement;
 
   //Die Funktion, die auf Aktiviäten auf der Seite hört//
   function handleload() {
-    document.addEventListener("mousemove", setInfoBox);
+    document.addEventListener("mousemove", setinfoBox);
     document.addEventListener("click", logInfo);
     document.addEventListener("keyup", logInfo);
-  
+
     const body = document.getElementsByTagName("body")[0];
     body.addEventListener("click", logInfo);
     body.addEventListener("keyup", logInfo);
-  
-      //Schleife, die sicherstellt, dass auf beide Div Elemente reagiert wird//
+
+    //Schleife, die sicherstellt, dass auf beide Div Elemente reagiert wird//
     const divs = document.getElementsByClassName("divs");
     for (let i = 0; i < divs.length; i++) {
       divs[i].addEventListener("click", logInfo);
       divs[i].addEventListener("keyup", logInfo);
     }
-  }
-  
+  };
 
-hi.addeventlistener ("click", function setinfoBox (_event:MouseEvent) {
+  //Inhalt des Span-Elements anzeigen//
+  function setinfoBox(_event: MouseEvent) {
+    const positionx: number = _event.clientX;
+    const positiony: number = _event.clientY;
 
+    let x: string = String(positionx + 15);
+    let y: string = String(positiony + 15);
 
-    });
+    let infobox = document.getElementById("thespan") as HTMLSpanElement;
+    infobox.innerHTML = "position x: " + x + " position y: " + y + " Event-Target: " + _event.target;
 
-    function logInfo () {
+    infobox.style.position = "fixed";
+    infobox.style.top = y + "px";
+    infobox.style.left = x + "px";
+  };
 
+  //Infos in der Konsole aufrufen//
+  function logInfo(_event: Event) {
 
-    };
+    console.log(_event.type + _event.target + _event.currentTarget + _event);
 
+  };
 
+  handleload();
 
-
-  });
-  
+});
