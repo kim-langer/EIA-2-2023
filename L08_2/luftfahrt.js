d3a995148b4763d91973424d54a92a5cf52d369b
kim-langer <kim.sophie.langer@hs-furtwangen.de>
2023-05-13 21:46:15 +0200

    update

--- L08_2/luftfahrt.js
+++ L08_2/luftfahrt.js
@@ -1,8 +1,48 @@
-var L082;
-(function (L082) {
-    function drawWindsocke(_pos, x, _pos, y) {
-        moveTo(_pos.x, _pos.y);
-        lineTo(_pos.x, _pos.y - 50);
+/*Aufgabe: L08.2
+Name: Kim Langer
+Matrikelnummer: 272232
+Quellen: Jirkas Inverted Classroom, ChatGPT
+*/
+var L08_2;
+(function (L08_2) {
+    window.addEventListener("load", handleLoad);
+    let canvas = document.querySelector('canvas');
+    let crc2 = canvas.getContext('2d');
+    function handleLoad(_event) {
+        drawBackground();
+        drawCloud();
     }
-})(L082 || (L082 = {}));
+    //Hintergrund definieren//
+    let horizon = 0.62;
+    function drawBackground() {
+        let gradient = crc2.createLinearGradient(0, 0, 0, crc2.canvas.height);
+        gradient.addColorStop(0, "lightblue");
+        gradient.addColorStop(horizon - 0.001, "white");
+        gradient.addColorStop(horizon, "darkgreen");
+        gradient.addColorStop(1, "darkgreen");
+        crc2.fillStyle = gradient;
+        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+    }
+    function drawCloud(_position, _size) {
+        let nParticles = 200;
+        let radiusParticle = 23;
+        let particle = new Path2D();
+        let gradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radiusParticle);
+        particle.arc(0, 0, radiusParticle, 0, 2 * Math.PI);
+        gradient.addColorStop(0, "HSLA(0, 100%, 100%, 0.5)");
+        gradient.addColorStop(1, "HSLA(0, 100%, 100%, 0)");
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        for (let drawn = 0; drawn < nParticles; drawn++) {
+            let x = (Math.random() - 0.5) * _size.x;
+            let y = -(Math.random()) * _size.y;
+            crc2.translate(x, y);
+            crc2.fill(particle);
+            crc2.restore();
+        }
+        crc2.restore();
+    }
+    // function drawWindsocke(_pos.x, _pos.y) {moveTo(_pos.x, _pos.y) lineTo(_pos.x, _pos.y - 50)//*
+})(L08_2 || (L08_2 = {}));
 //# sourceMappingURL=luftfahrt.js.map
--- L08_2/luftfahrt.ts
+++ L08_2/luftfahrt.ts
@@ -1,23 +1,64 @@
 /*Aufgabe: L08.2
 Name: Kim Langer
 Matrikelnummer: 272232
-Quellen: ChatGPT
+Quellen: Jirkas Inverted Classroom, ChatGPT
 */
-namespace L082 {
-    const canvas = document.querySelector('canvas');
-    const crc2 = canvas.getContext('2d');
+namespace L08_2 {
 
-    // Darstellung des Bildes responsive gestalten/ smartphoneoptimiert
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    window.addEventListener("load", handleLoad);
 
+    let canvas = document.querySelector('canvas');
+    let crc2 = canvas.getContext('2d');
 
-    function drawWindsocke(_pos.x, _pos.y) {
-
-        moveTo(_pos.x, _pos.y)
-        lineTo(_pos.x, _pos.y - 50)
+    function handleLoad(_event: Event): void {
+        drawBackground();
+        drawCloud();
 
     }
+
+    //Hintergrund definieren//
+    let horizon: number = 0.62;
+    function drawBackground(): void {
+        let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 0, crc2.canvas.height);
+        gradient.addColorStop(0, "lightblue");
+        gradient.addColorStop(horizon - 0.001, "white");
+        gradient.addColorStop(horizon, "darkgreen");
+        gradient.addColorStop(1, "darkgreen");
+
+        crc2.fillStyle = gradient;
+        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+    }
+
+
+    function drawCloud(_position: Vector, _size: Vector): void {
+
+        let nParticles: number = 200;
+        let radiusParticle: number = 23;
+        let particle: Path2D = new Path2D();
+        let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radiusParticle)
+
+        particle.arc(0, 0, radiusParticle, 0, 2 * Math.PI);
+        gradient.addColorStop(0, "HSLA(0, 100%, 100%, 0.5)");
+        gradient.addColorStop(1, "HSLA(0, 100%, 100%, 0)");
+
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+  crc2.fillStyle = gradient;
+
+        for(let drawn: number= 0 ; drawn < nParticles; drawn++) {
+            let x: number = (Math.random() -0.5) * _size.x;
+            let y: number = -(Math.random()) * _size.y;
+            crc2.translate(x,y);
+            crc2.fill(particle);
+            crc2.restore();
+        }
+
+        crc2.restore();
+
+    }
+
+
+    // function drawWindsocke(_pos.x, _pos.y) {moveTo(_pos.x, _pos.y) lineTo(_pos.x, _pos.y - 50)//*
 
 
 
